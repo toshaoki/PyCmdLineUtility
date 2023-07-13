3734868d6c9f2b05e51dff03bd4ae2956a9afb18
Toshihiro AOKI <kelloggs.a.t.19@gmail.com>
2023-07-13 03:12:38 +0000

    feat: Add feature for substituting patterns in LaTeX files
    - Created a new file `latex_substitutor.py` with functions for pattern substitution in text files
    - Implemented `substitute_patterns` to replace specific patterns using regular expressions
    - Added support for replacing patterns like "stem:[...]" and "latexmath:[...]" with "$some words$"

---
+++ latex_substitutor.py
@@ -0,0 +1,35 @@
+import re
+import sys
+
+def substitute_patterns(text):
+    """
+    Replace specific patterns in the given text.
+
+    Args:
+        text (str): The text to process.
+
+    Returns:
+        str: The text with "stem:[ some words]" and "latexmath:[ some words]" replaced by "$some words$".
+    """
+    pattern = r'(stem|latexmath):\[\s*(.*?)\s*\]'
+    return re.sub(pattern, r'$\2$', text)
+
+def process_file(filename):
+    """
+    Read a file, replace specific patterns in its content, and overwrite the file with the new content.
+
+    Args:
+        filename (str): The name of the file to process.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+    
+    new_text = substitute_patterns(text)
+    
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(new_text)
+
+# Get filename from command line arguments
+filename = sys.argv[1]
+
+process_file(filename)
