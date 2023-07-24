9539fd0607efe12852d71248e539a815bb0f210c
Toshihiro AOKI <kelloggs.a.t.19@gmail.com>
2023-07-24 09:23:14 +0000

    feat: Add Latex to Asciidoctor inverse substitution functionality
    - Add new function `inverse_substitute_patterns` to `adoc_inverse_substitutor.py`
    - Add new function `process_file` to `adoc_inverse_substitutor.py`
    - Retrieve `filename` from command line arguments in `adoc_inverse_substitutor.py`

--- adoc_inverse_substitutor.py
+++ adoc_inverse_substitutor.py
@@ -1,6 +1,5 @@
 import re
 import sys
-from adoc_substitutor import process_file
 
 def inverse_substitute_patterns(text):
     """
@@ -16,6 +15,23 @@ def inverse_substitute_patterns(text):
     """
     pattern = r'\$\s*(.*?)\s*\$'
     return re.sub(pattern, r'stem:[\1]', text)
+
+
+def process_file(filename):
+    """
+    Read a file, replace specific patterns in its content, and overwrite the file with the new content.
+
+    Args:
+        filename (str): The name of the file to process.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        text = file.read()
+    
+    new_text = inverse_substitute_patterns(text)
+    
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(new_text)
+
 
 # Get filename from command line arguments
 filename = sys.argv[1]
